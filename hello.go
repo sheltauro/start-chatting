c3a4361acb67d1a39c87bfbd1cca4aea17cfa372
RC <33100403+aarcee141@users.noreply.github.com>
2023-03-25 19:27:56 -0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var clients []websocket.Conn
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		clients = append(clients, *conn)
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			for _, client := range clients {
+				// Write message back to browser
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					return
+				}
+			}
+
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe("localhost:8080", nil)
+	fmt.Print("Started server and listening at 8080")
+}
